84c90c06f561a701fdc96850b7bbe26458efae34
GerardoDRM <gerardo.delarosama@udlap.mx>
2017-05-15 00:38:18 -0500

    Convocations list

--- app/src/main/java/com/craftcode/android/clicksocial/ConvocationsFragment.java
+++ app/src/main/java/com/craftcode/android/clicksocial/ConvocationsFragment.java
@@ -1,34 +1,49 @@
 package com.craftcode.android.clicksocial;
 
 import android.os.Bundle;
-import android.support.design.widget.BottomSheetDialogFragment;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.craftcode.android.clicksocial.adapter.SupportListAdapter;
-import com.craftcode.android.clicksocial.models.Support;
-import com.getbase.floatingactionbutton.FloatingActionButton;
+import com.craftcode.android.clicksocial.API.ConvocationsApi;
+import com.craftcode.android.clicksocial.adapter.ConvocationsListAdapter;
+import com.craftcode.android.clicksocial.models.Convocation;
+import com.craftcode.android.clicksocial.models.ConvocationResults;
+import com.craftcode.android.clicksocial.utils.GeneralConst;
 
 import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import retrofit.Call;
+import retrofit.Callback;
+import retrofit.GsonConverterFactory;
+import retrofit.Response;
+import retrofit.Retrofit;
 
 
 public class ConvocationsFragment extends Fragment {
 
 
+    private String BASE_URL;
+    private ConvocationsApi apiService;
+    private ConvocationsListAdapter adapter;
     @Bind(R.id.recycler_view)
     RecyclerView mRecyclerView;
-    @Bind(R.id.fab_opt)
-    FloatingActionButton mFab;
+    private ArrayList<Convocation> mConvocations;
 
     public ConvocationsFragment() {
         // Required empty public constructor
+        this.BASE_URL = GeneralConst.BASE_URL;
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        apiService = retrofit.create(ConvocationsApi.class);
     }
 
     @Override
@@ -45,19 +60,6 @@ public class ConvocationsFragment extends Fragment {
 
         // Create Dummy Data on RecyvlerView
         setUpSupportList();
-        // Add button action
-        mFab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Show filters
-                BottomSheetDialogFragment bottomSheetDialogFragment = new FilterActivity();
-                Bundle bundle = new Bundle();
-                bundle.putInt("FILTEROPT", 1);
-                bottomSheetDialogFragment.setArguments(bundle);
-                bottomSheetDialogFragment.show(getFragmentManager(), bottomSheetDialogFragment.getTag());
-            }
-        });
-
         return rootView;
     }
 
@@ -67,31 +69,49 @@ public class ConvocationsFragment extends Fragment {
      */
     private void setUpSupportList() {
         // In order to create a dummy data we use a loop
-        ArrayList<Support> supports = new ArrayList<Support>();
-        String[] urls = {
-                "http://www.youngmarketing.co/wp-content/uploads/2016/04/i_Stock_000052774698_Small-693x325.jpg",
-                "https://userscontent2.emaze.com/images/395d1462-3a3e-4fcb-94c5-4917dfefef47/853865f9-647e-45ef-ab38-6862556cac6a.png",
-                "http://www.estrategiaynegocios.net/csp/mediapool/sites/dt.common.streams.StreamServer.cls?STREAMOID=IwxOPBGLjsRcJjztt_HDC8$daE2N3K4ZzOUsqbU5sYskQgxe5dUujHkNqAWR2SqKWCsjLu883Ygn4B49Lvm9bPe2QeMKQdVeZmXF$9l$4uCZ8QDXhaHEp3rvzXRJFdy0KqPHLoMevcTLo3h8xh70Y6N_U_CryOsw6FTOdKL_jpQ-&CONTENTTYPE=image/jpeg"
-        };
-        String[] models = {"Municipal", "Estatal", "Federal"};
-
-        for (int i=0; i<8; i++) {
-            supports.add(new Support("Apoyo " + i, models[(int)(Math.random() * (3) + 0)], "Descripcion", "7/11/2016", "7/12/2016", urls[(int)(Math.random() * (3) + 0)]));
-        }
-        // Create Adapter in order to create list with fake data
-        SupportListAdapter adapter = new SupportListAdapter(getContext(), supports);
-
+// Create Adapter in order to create list with fake data
+        adapter = new ConvocationsListAdapter(getContext(), new ArrayList<Convocation>());
 
         // Create recycler view with dynamic height behavior
-        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getContext());
+        final StaggeredGridLayoutManager grid =
+                new StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL);
 
-        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setLayoutManager(grid);
         mRecyclerView.setHasFixedSize(true);
         mRecyclerView.setItemViewCacheSize(20);
         mRecyclerView.setDrawingCacheEnabled(true);
         mRecyclerView.setDrawingCacheQuality(View.DRAWING_CACHE_QUALITY_HIGH);
+        mRecyclerView.setLayoutManager(grid);
 
         mRecyclerView.setAdapter(adapter);
+
+        if (mConvocations == null) {
+            mConvocations = new ArrayList<Convocation>();
+            getConvocations();
+        }
+
+    }
+
+    private void getConvocations() {
+        Call<ConvocationResults> call = apiService.getConvocations();
+        call.enqueue(new Callback<ConvocationResults>() {
+
+            @Override
+            public void onResponse(Response<ConvocationResults> response, Retrofit retrofit) {
+                int statusCode = response.code();
+                ConvocationResults convovations = response.body();
+                if (convovations == null) return;
+                mConvocations.addAll(convovations.getResults());
+                ArrayList<Convocation> tmpPromo = new ArrayList<Convocation>(mConvocations);
+                adapter.refill(tmpPromo);
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                if (!GeneralConst.checkNetwork(getContext()))
+                    GeneralConst.showMessageConnection(getContext());
+            }
+        });
 
     }
 
---
+++ app/src/main/java/com/craftcode/android/clicksocial/adapter/ConvocationsListAdapter.java
@@ -0,0 +1,100 @@
+package com.craftcode.android.clicksocial.adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.craftcode.android.clicksocial.ConvocationActivity;
+import com.craftcode.android.clicksocial.R;
+import com.craftcode.android.clicksocial.models.Convocation;
+import com.squareup.picasso.Picasso;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.TimeZone;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+/**
+ * Dummy adapter for Campus Party contest
+ * This Adapter create instances on recyclerview
+ */
+public class ConvocationsListAdapter extends RecyclerView.Adapter<ConvocationsListAdapter.ViewHolder> {
+
+    private Context mContext;
+    private ArrayList<Convocation> mConvocations;
+
+    public ConvocationsListAdapter(Context context, ArrayList<Convocation> events) {
+        this.mContext = context;
+        this.mConvocations = events;
+    }
+
+    @Override
+    public ConvocationsListAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_convocation, parent, false);
+        view.setFocusable(true);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.mTitle.setText(mConvocations.get(position).getTitle());
+        holder.mDescription.setText(mConvocations.get(position).getDescription());
+        Picasso.with(mContext).load(mConvocations.get(position).getImg()).fit().centerCrop().tag(mContext).into(holder.mThumbnail);
+
+        SimpleDateFormat sds = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+
+        sds.setTimeZone(TimeZone.getDefault());
+        long event_date = mConvocations.get(position).getCreation_date();
+        final Date date_s = new Date(event_date * 1000L);
+        holder.mCreation.setText(sds.format(date_s));
+
+        holder.mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(mContext, ConvocationActivity.class);
+                mContext.startActivity(i);
+
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mConvocations.size();
+    }
+
+    public void refill(List<Convocation> convocations) {
+        mConvocations.clear();
+        mConvocations.addAll(convocations);
+        notifyDataSetChanged();
+    }
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        @Bind(R.id.event_title)
+        TextView mTitle;
+        @Bind(R.id.event_description) TextView mDescription;
+        @Bind(R.id.thumbnail)
+        ImageView mThumbnail;
+        @Bind(R.id.creation)
+        TextView mCreation;
+        public View mView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            mView = itemView;
+        }
+    }
+}
